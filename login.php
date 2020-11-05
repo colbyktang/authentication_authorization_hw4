b73d4f3728af9ac233eb9622143b55263aa41b3d
colbyktang <colby.us2012@gmail.com>
2020-11-05 12:22:57 -0600

    semicolon

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "COSC4343";
-$dbname = "UserAccounts"
+$dbname = "UserAccounts";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
