43b4c71fcff80b49f552231506cbc7fbd0992394
colbyktang <colby.us2012@gmail.com>
2020-11-05 12:21:48 -0600

    Testing database SELECT

--- login.php
+++ login.php
@@ -2,13 +2,28 @@
 $servername = "localhost";
 $username = "root";
 $password = "COSC4343";
+$dbname = "UserAccounts"
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
 echo "Connected successfully";
+
+$sql = "SELECT username, password FROM UserAccounts";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "Username/Password: " . $row["username"]. " " . $row["password"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+}
 ?>
