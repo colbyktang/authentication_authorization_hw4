975319adbe8374049c5f6e6c6121ea39c4044f53
colbyktang <colby.us2012@gmail.com>
2020-11-06 05:01:27 -0600

    Added captcha

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $servername = "localhost";
 $username = "root";
 $password = "COSC4343";
@@ -18,6 +20,20 @@ if ($conn->connect_error) {
 echo "Connected successfully<br>";
 echo "login_username: " . $login_username . "<br>";
 echo "login_password: " . $login_password . "<br>";
+
+include_once $_SERVER['DOCUMENT_ROOT'] . '/securimage/securimage.php';
+$securimage = new Securimage();
+
+if ($securimage->check($_POST['captcha_code']) == false) {
+    // the code was incorrect
+    // you should handle the error so that the form processor doesn't continue
+  
+    // or you can use the following code if there is no validation or you do not know how
+    echo "The security code entered was incorrect.<br /><br />";
+    echo "Please go <a href='javascript:history.go(-1)'>back</a> and try again.";
+    exit;
+}
+
 
 $sql = "SELECT username, password FROM user_accounts WHERE username='$login_username' AND password='$login_password'";
 echo "sql: " . $sql . "<br>";
