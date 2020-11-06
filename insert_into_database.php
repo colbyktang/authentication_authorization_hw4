8f8afa66d8759f16006023bbe73a8d44da7fd5e6
colbyktang <colby.us2012@gmail.com>
2020-11-06 03:28:49 -0600

    insert into db

--- insert_into_database.php
+++ insert_into_database.php
@@ -4,9 +4,6 @@ $username = "root";
 $password = "COSC4343";
 $dbname = "user_accounts";
 
-$login_username = $_POST['username'];
-$login_password = $_POST['password'];
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -16,10 +13,17 @@ if ($conn->connect_error) {
 }
 
 echo "Connected successfully<br>";
-echo "login_username: " . $login_username . "<br>";
-echo "login_password: " . $login_password . "<br>";
 
-$sql = "SELECT username, password FROM user_accounts WHERE username='$login_username' AND password='$login_password'";
+$sql = "INSERT INTO user_accounts (userID, username, password, clearance) VALUES (1, 'alice', '@1!c3', 'T')";
+echo "sql: " . $sql . "<br>";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
+}
+
+$sql = "SELECT username, password FROM user_accounts";
 echo "sql: " . $sql . "<br>";
 $result = $conn->query($sql);
 
@@ -27,10 +31,11 @@ if ($result->num_rows > 0) {
     // output data of each row
 
     while ($row = $result->fetch_assoc()) {
-        echo "Username/Password: " . $row["username"]. " " . $row["password"]. "<br>";
+        echo "Username/Password: " . $row["username"]. " " . $row["password"] . "<br>";
     }
 }
 else {
     echo "0 results";
 }
+
 ?>
