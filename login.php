c5c879c9de54470a8e94976aadfcf34c63078e7d
colbyktang <colby.us2012@gmail.com>
2020-11-06 00:27:42 -0600

    changed db name

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "COSC4343";
-$dbname = "UserAccounts";
+$dbname = "user_accounts";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
