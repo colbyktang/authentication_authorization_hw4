c1fcfae5715290a3276ea587de4a552ba1d09501
colbyktang <colby.us2012@gmail.com>
2020-11-06 01:27:07 -0600

    changing fetch

--- login.php
+++ login.php
@@ -22,7 +22,7 @@ if ($result->num_rows > 0) {
     // this while loop does not work, investigate fetch_assoc()
     echo $result;
 
-    while ($row = $result->fetch_array()) {
+    while ($row = $result->fetch_assoc()) {
         echo "Username/Password: " . $row["username"]. " " . $row["password"]. "<br>";
     }
 }
